316fe796d795596bfdfc18da2ec820ba91141750
BrownEdgar <nikogosjanedgar@gmail.com>
2023-09-01 19:27:41 +0400

    Gohar homework done (test)

--- src/Users/Gohar/App.jsx
+++ src/Users/Gohar/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function App() {
+	const [count, setCount] = useState(0)
 	return (
 		<div>
-			<h1>Lorem ipsum dolor sit.</h1>
+			<h1>Lorem ipsum dolor sit. {count}</h1>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, laudantium alias qui nemo commodi fuga? Impedit natus quisquam, accusamus, mollitia nesciunt id eos ipsam veritatis iste voluptatum, expedita voluptate eveniet.</p>
 		</div>
 	)
 }
