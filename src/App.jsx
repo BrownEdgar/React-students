bb4e21d7ee84d4cce772dc7e9d61124b2f1b92d0
BrownEdgar <nikogosjanedgar@gmail.com>
2023-09-01 19:19:41 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+
+export default function App() {
+	return (
+		<div>
+			<h1>Main App</h1>
+		</div>
+	)
+}
---
+++ src/Users/Gohar/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+	return (
+		<div>
+			<h1>Lorem ipsum dolor sit.</h1>
+		</div>
+	)
+}
