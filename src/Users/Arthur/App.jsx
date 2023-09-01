1a473f4734675dcffb80f35c45e251932dbc3795
BrownEdgar <nikogosjanedgar@gmail.com>
2023-09-01 19:53:27 +0400

    fix some code

--- src/Users/Arthur/App.jsx
+++ src/Users/Arthur/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+
 
 export default function App() {
 	return (
 		<div>
-			<h1>Lorem ipsum dolor sit.</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem repellendus possimus non temporibus voluptatibus quas nobis placeat soluta. Autem, sapiente dolor, voluptate laudantium cumque ipsa cum dolorem placeat ab facilis fuga dicta dolore ea officia doloremque deleniti inventore exercitationem obcaecati!</p>
 		</div>
 	)
 }
