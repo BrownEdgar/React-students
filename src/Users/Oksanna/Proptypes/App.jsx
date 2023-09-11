0a01757a10c7bbf28cae2d4fc2b1f1a1ee122dcd
SanaSargsyan <sanasargsyan13@gmail.com>
2023-09-11 16:03:20 +0400

    propTypes lesson

---
+++ src/Users/Oksanna/Proptypes/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Child from './Child'
+
+export default function App() {
+  return (
+    <div>
+      <Child age={67} gender='male' todo={{id:2, title: 'jhgf', x: 1}}/>
+    </div>
+  )
+}
