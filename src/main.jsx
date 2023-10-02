787be0d01797ea29bb21393d12a4e1d02c3bc6ce
Grish Khechoyan <grishkhechoyan1@gmail.com>
2023-10-03 00:36:35 +0400

    Menu2 Example

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Grish/Components/Menuu/App";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
